d16768bda32f58ec14238e1ffff71af8e9557e4d
Lenora Porter <lporter630@gmail.com>
2019-05-31 22:37:58 -0400

    Add Week 2

---
+++ Week2/react-to-do/src/App.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function Todo({ todo, index, completeTodo, removeTodo }) {
+  return (
+    <React.Fragment>
+      <div className="row">
+        <div className="complete" onClick={() => completeTodo(index)}>
+          <a>
+            <i class="fa fa-check"></i>
+          </a>
+        </div>
+        <div className="todo" style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
+          {todo.text}
+        </div>
+        <div className="trash" onClick={() => removeTodo(index)}>
+          <a>
+            <i class="fa fa-trash"></i>
+          </a>
+        </div>
+    </div>
+    </React.Fragment>
+  );
+}
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Learn about React",
+      isCompleted: false
+    },
+    {
+      text: "Meet friend for lunch",
+      isCompleted: false
+    },
+    {
+      text: "Build really cool todo app",
+      isCompleted: false
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+      <h2 class="todo-list-title">Todo List</h2>
+      <p class="todo-list-sub-title">Stuff I need to do for #React100</p>  
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
